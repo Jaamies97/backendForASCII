c1f4eab5c3fd0931c24f3f98f78b608e49c4b7c5
Jaamies97 <axel.ojutkangas@gmail.com>
2021-10-28 02:48:05 +0300

    sain hyvän backending

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/axel_ojut/<username>')
-def hello_world(username):
-    return f'<h1>Hello Axel and {username}!</h1>'
+
+@app.route('/axel_ojut', methods=['GET'])
+def convertionfstr():
+    sumofascii = 0
+    dictionary = {"1": {},
+                  "2": {}}
+    inputstr = str(request.args['query'])
+    for char in inputstr:
+        sumofascii += ord(char)
+        if char not in dictionary["2"]:
+            dictionary["2"][char] = 1
+        else:
+            dictionary["2"][char] += 1
+
+    dictionary["1"] = str(sumofascii)
+    return dictionary
+
 
 if __name__ == '__main__':
     app.run()
